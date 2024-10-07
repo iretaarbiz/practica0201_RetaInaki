89db7098b76c3becfd2518ff2aa8e7f4d8eeb3d4
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-10-07 11:49:22 +0200

    Ejercicio 3
    Subo ejercicio 3

---
+++ Ejercicio3.py
@@ -0,0 +1,4 @@
+#Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca 
+#muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+nom = input("Cual es tu nombre?")
+print("¡Hola ", nom,"!",sep="")
